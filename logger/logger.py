b59e6e760e58c3173f0b7f8cbf7dad1c24316fb7
bobwww <None>
2022-05-27 17:56:54 +0300

    Packet logger

--- logger/logger.py
+++ logger/logger.py
@@ -1,23 +1,40 @@
-import json
-import datetime as dt
-from scapy.all import hexdump
-
-def verdict_to_json(verdict):
-    return json.dumps({
-        'time': dt.datetime.strftime('%Y-%m-%d::%H:%M:%S'),
-        'payload': hexdump(verdict.pkt),
-        'alert': verdict.alert,
-        'sent': verdict.send
-    })
+from pymongo import MongoClient
+from datetime import datetime
+import time
+from scapy.all import raw
 
 
 class Logger:
 
-    def __init__(self, path):
-        self.fd = open(path, 'a')
+    def __init__(self, uri):
+        self.client = MongoClient(uri)
 
-    def log(self, verdict):
-        self.fd.write(verdict_to_json(verdict) + '\n')
+    def log(self, verdict, timestamp: float=None):
+        # Create the document to be stored
+        docm = {
+            'layers': [name for name in self.get_packet_layers(verdict.pkt)],
+            'raw': raw(verdict.pkt),
+            'timestamp': time if time else time.time(),
+            'alert': verdict.alert,
+            'sent': verdict.send
+        }
+        # Get the collection for the current time (by hour)
+        # Uses GMT+0 time for consistency
+        now = datetime.utcnow().strftime('%Y%m%d%H')
+        coll = self.client[now]
+        # Insert document
+        coll.insert_one(docm)
 
-    def __del__(self):
-        self.fd.close()
+    @staticmethod
+    def get_packet_layers(packet):
+        '''
+        Given a packet, generates its layers' names.
+        '''
+        counter = 0
+        while True:
+            layer = packet.getlayer(counter)
+            if layer is None:
+                break
+
+            yield layer
+            counter += 1
