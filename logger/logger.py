94c2fe62ba7435a59eedb58d2b9e5df29b8da836
bobwww <None>
2022-05-28 14:51:17 +0300

    Finished logger, created database reader

--- logger/logger.py
+++ logger/logger.py
@@ -1,40 +1,25 @@
 from pymongo import MongoClient
 from datetime import datetime
-import time
 from scapy.all import raw
+from utils import get_packet_layers
 
-
+    
 class Logger:
 
-    def __init__(self, uri):
+    def __init__(self, uri: str) -> None:
+        # Establishes connection to database
         self.client = MongoClient(uri)
+        self.coll = self.db.coll
 
-    def log(self, verdict, timestamp: float=None):
-        # Create the document to be stored
-        docm = {
-            'layers': [name for name in self.get_packet_layers(verdict.pkt)],
-            'raw': raw(verdict.pkt),
-            'timestamp': time if time else time.time(),
-            'alert': verdict.alert,
-            'sent': verdict.send
-        }
-        # Get the collection for the current time (by hour)
-        # Uses GMT+0 time for consistency
-        now = datetime.utcnow().strftime('%Y%m%d%H')
-        coll = self.client[now]
-        # Insert document
-        coll.insert_one(docm)
-
-    @staticmethod
-    def get_packet_layers(packet):
-        '''
-        Given a packet, generates its layers' names.
-        '''
-        counter = 0
-        while True:
-            layer = packet.getlayer(counter)
-            if layer is None:
-                break
-
-            yield layer
-            counter += 1
+    def log(self, verdict, utctime: datetime=None) -> None:
+        """
+        Logs the packet and its metadata.
+        """
+        self.coll.insert_one(
+                {
+                'raw': raw(verdict.pkt),
+                'layers': [name for name in get_packet_layers(verdict.pkt)],
+                'time': utctime if utctime else datetime.utcnow(),
+                'flags': {'alert': verdict.alert, 'sent': verdict.send}
+                }
+        )
